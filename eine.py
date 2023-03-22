3f5222048464d0b873523966399099ff5d053551
mubashirusman <82461283+MubashirUsman@users.noreply.github.com>
2023-03-22 15:02:23 +0000

    Formatting and added some commands

--- eine.py
+++ eine.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+COMMAND= [
+  {
+    'id': 1,
+    'Name': "lsblk",
+    'used': 'list block devices'
+  },
+    {
+    'id': 2,
+    'Name': "fdisk",
+    'used': 'show detailed information about block devices'
+  },
+    {
+    'id': 3,
+    'Name': "mount",
+    'used': 'mount a partition'
+  },
+    {
+    'id': 4,
+    'Name': "uname -r",
+    'used': 'kernal version'
+  }
+]
 @app.route("/")
 def helloWorld():
-  return render_template('home.html')
+  return render_template('home.html', command=COMMAND)
 
 
 if __name__ == "__main__":
